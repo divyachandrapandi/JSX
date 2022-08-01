2af04fa2d8eeff1c4f18aad8821ff3e35595261a
Divya chandrapandi <98633621+divyachandrapandi@users.noreply.github.com>
2022-08-01 09:23:41 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+//  React works by creating this JSX files
+//  A Babel is react compiler compiles the html element inside js file
+// ReactDOM.render(WHAT TO SHOW, WHERE TO SHOW, OPTIONAL CALLBACK);
+ReactDOM.render(<h1>Hello Rejo</h1>, document.querySelector("#root"));
